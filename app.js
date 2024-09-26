ac27bf3c545ff3ccda65a393d9271105bbb73165
Abdalla Alhag <alhagabdalla@gmail.com>
2024-09-25 19:09:27 -0700

    Add PORT env

--- app.js
+++ app.js
@@ -123,5 +123,5 @@ app.use((err, req, res, next) => {
     next(err);
 });
 
-app.listen(3000, () => console.log("app listening on port 3000!"));
-
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`app listening on port ${PORT}!`));
